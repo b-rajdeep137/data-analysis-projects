8add595fd1dbf67a758baaa53bd2961026329ffb
b-rajdeep137 <ardie137@gmail.com>
2025-10-20 22:04:42 -0500

    Final before submission

--- sql-part-3/studio/Studio_3.sql
+++ sql-part-3/studio/Studio_3.sql
@@ -1,3 +1,5 @@
+
+/*
 SELECT TOP 10 t.tag_name, bt.tag_id, SUM(bt.count) AS total
 FROM BooksDB.dbo.book_tags AS bt
 INNER JOIN BooksDB.dbo.tags AS t 
@@ -40,4 +42,21 @@ LEFT JOIN BooksDB.dbo.books AS b
 ON bt.goodreads_book_id = b.book_id 
 GROUP BY t.tag_name, bt.tag_id, b.title
 HAVING t.tag_name LIKE '%spring'
+ORDER BY total DESC
+*/
+
+SELECT  count(t.tag_name) as "total count",t.tag_name, bt.tag_id
+FROM BooksDB.dbo.book_tags AS bt
+INNER JOIN BooksDB.dbo.tags AS t 
+ON bt.tag_id = t.tag_id
+GROUP BY t.tag_name, bt.tag_id
+HAVING t.tag_name LIKE '%halloween%'
+ORDER BY count(t.tag_name) DESC
+
+SELECT TOP 10  t.tag_name, bt.tag_id, SUM(bt.count) AS total
+FROM BooksDB.dbo.book_tags AS bt
+INNER JOIN BooksDB.dbo.tags AS t 
+ON bt.tag_id = t.tag_id
+GROUP BY t.tag_name, bt.tag_id
+HAVING t.tag_name LIKE '%smile%'
 ORDER BY total DESC
